e8b954498e8202907c19ca49c1b9c98aaf3b35c0
iqbal rubel <iqbalict39@outlook.com>
2018-04-20 00:08:49 +0600

    server.js modified

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-var express = require('express.io');
-var app = express();
+const express=require('express.io');
+var app=express();
 app.http().io();
-var PORT = 3000;
-console.log('server started on port ' + PORT);
+var PORT=3000;
+console.log('Server Started on PORT'+PORT);
+app.use(express.static(__dirname+'/public'));
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-	res.render('index.ejs');
+app.get('/',function(req,res){
+  res.render('index.ejs');
 });
 
+app.io.route('ready',function(req){
+  req.io.join(req.data.chat_room);
+  req.io.join(req.data.signal_room);
+  app.io.room(req.data).broadcast('announce',{
+    message:'New client in the '+req.data+'room'
+  })
+})
+
+app.io.route('send',function(req){
+  app.io.room(req.data.room).broadcast('message',{
+    message:req.data.message,
+    author:req.data.author
+  })
+})
+
+app.io.route('signal',function(req){
+  //note the use of req here for broadcasting so only the sender doesnt receive their own messages
+req.io.room(req.data.room).broadcast('signaling_message',{
+  type:req.data.type,
+  message:req.data.message
+})
+})
 app.listen(PORT);
-
-app.io.route('ready', function(req) {
-	req.io.join(req.data.chat_room);
-	req.io.join(req.data.signal_room);
-	app.io.room(req.data).broadcast('announce', {
-		message: 'New client in the ' + req.data + ' room.'
-	})
-})
-
-app.io.route('send', function(req) {
-    app.io.room(req.data.room).broadcast('message', {
-        message: req.data.message,
-		author: req.data.author
-    });
-})
-
-app.io.route('signal', function(req) {
-	//Note the use of req here for broadcasting so only the sender doesn't receive their own messages
-	req.io.room(req.data.room).broadcast('signaling_message', {
-        type: req.data.type,
-		message: req.data.message
-    });
-})
